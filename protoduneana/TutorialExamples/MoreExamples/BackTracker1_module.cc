5ec7de3e685623dfeb557d16ee367f71d1e6d4e0
Tingjun Yang <tjyang@fnal.gov>
2022-05-01 08:14:54 -0500

    Save number of points in 14 cm segments.

--- protoduneana/TutorialExamples/MoreExamples/BackTracker1_module.cc
+++ protoduneana/TutorialExamples/MoreExamples/BackTracker1_module.cc
@@ -50,9 +50,14 @@ public:
 
 private:
 
+  // distance between reconstructed trajectory points and true energy deposition location
   TH1D *hdeltax;
   TH1D *hdeltay;
   TH1D *hdeltaz;
+
+  // number of points in 14 cm segments
+  TH1D *hsegpts_reco;
+  TH1D *hsegpts_true;
 
 };
 
@@ -90,24 +95,92 @@ void pdsp::BackTracker1::analyze(art::Event const& e)
 
     auto & track = tracklist[trkIter];
 
+    //Ignore short tracks
+    if (track->Length()<10) continue;
+    
     auto vhit = fmthm.at(trkIter);
     auto vmeta = fmthm.data(trkIter);
 
+    int nsegpts_reco = 0;
+    int nsegpts_true = 0;
+    double seglength_reco = 0;
+    double seglength_true = 0;
+    double prevx_reco = 0;
+    double prevy_reco = 0;
+    double prevz_reco = 0;
+    double prevx_true = 0;
+    double prevy_true = 0;
+    double prevz_true = 0;
     for (size_t ip = 0; ip<track->NPoints(); ++ip){
       // Ignore invalid trajectory points
       if (!track->HasValidPoint(ip)) continue;
       auto &loc = track->LocationAtPoint(ip);
+      //find number of points in reco segments of 14 cm
+      static bool isfirst_reco = true;
+      if (isfirst_reco){
+        prevx_reco = loc.X();
+        prevy_reco = loc.Y();
+        prevz_reco = loc.Z();
+        nsegpts_reco = 1;
+        isfirst_reco = false;
+      }
+      else{
+        double dis = sqrt(pow(loc.X() - prevx_reco, 2)+
+                          pow(loc.Y() - prevy_reco, 2)+
+                          pow(loc.Z() - prevz_reco, 2));
+        seglength_reco += dis;
+        if (seglength_reco>14){
+          hsegpts_reco->Fill(nsegpts_reco);
+          //cout<<"Fill reco segment "<<nsegpts_reco<<endl;
+          nsegpts_reco = 0;
+          seglength_reco = 0;
+        }
+        ++nsegpts_reco;
+        prevx_reco = loc.X();
+        prevy_reco = loc.Y();
+        prevz_reco = loc.Z();
+      }
+      //cout<<"reco x "<<loc.X()<<" reco y "<<loc.Y()<<" reco z "<<loc.Z()<<" seglength_reco "<<seglength_reco<<" nsegpts_reco "<<nsegpts_reco<<endl;
       for (size_t ii = 0; ii < vhit.size(); ++ii) {
         // Look for hit corresponding to the trajectory point
         if (vmeta[ii]->Index() == ip){
           auto simIDEs = bt_serv->HitToSimIDEs_Ps(clock_data, vhit[ii]);
           if (!simIDEs.empty()){
             // Find true location of the hit
+//            for (auto const& ide : simIDEs){
+//              cout<<"id = "<<ide->trackID<<" "<<ide->numElectrons<<" "<<ide->x<<" "<<ide->y<<" "<<ide->z<<" "<<endl;
+//            }
             auto xyz = bt_serv->HitToXYZ(clock_data, vhit[ii]);
             //cout<<loc.X()<<" "<<loc.Y()<<" "<<loc.Z()<<" "<<xyz[0]<<" "<<xyz[1]<<" "<<xyz[2]<<" "<<string(vhit[ii]->WireID())<<endl;
             hdeltax->Fill(loc.X() - xyz[0]);
             hdeltay->Fill(loc.Y() - xyz[1]);
             hdeltaz->Fill(loc.Z() - xyz[2]);
+            //find number of points in reco segments of 14 cm
+            static bool isfirst_true = true;
+            if (isfirst_true){
+              prevx_true = xyz[0];
+              prevy_true = xyz[1];
+              prevz_true = xyz[2];
+              nsegpts_true = 1;
+              isfirst_true = false;
+            }
+            else{
+              double dis = sqrt(pow(xyz[0] - prevx_true, 2)+
+                                pow(xyz[1] - prevy_true, 2)+
+                                pow(xyz[2] - prevz_true, 2));
+              seglength_true += dis;
+              if (seglength_true>14){
+                hsegpts_true->Fill(nsegpts_true);
+                //cout<<"Fill true segment "<<nsegpts_true<<endl;
+                nsegpts_true = 0;
+                seglength_true = 0;
+              }
+              ++nsegpts_true;
+              prevx_true = xyz[0];
+              prevy_true = xyz[1];
+              prevz_true = xyz[2];
+            }
+            //cout<<"true x "<<xyz[0]<<" true y "<<xyz[1]<<" true z "<<xyz[2]<<" seglength_true "<<seglength_true<<" nsegpts_true "<<nsegpts_true<<endl;
           }
         }
       }
@@ -121,6 +194,8 @@ void pdsp::BackTracker1::beginJob(){
   hdeltax = tfs->make<TH1D>("hdeltax", ";Reco x - true x (cm);N trajectory points",100,-2,2);
   hdeltay = tfs->make<TH1D>("hdeltay", ";Reco y - true y (cm);N trajectory points",100,-2,2);
   hdeltaz = tfs->make<TH1D>("hdeltaz", ";Reco z - true z (cm);N trajectory points",100,-2,2);
+  hsegpts_reco = tfs->make<TH1D>("hsegpts_reco", "Reco segments;Number of points;Segments", 200,0,200);
+  hsegpts_true = tfs->make<TH1D>("hsegpts_true", "True segments;Number of points;Segments", 200,0,200);
 }
 
 DEFINE_ART_MODULE(pdsp::BackTracker1)
