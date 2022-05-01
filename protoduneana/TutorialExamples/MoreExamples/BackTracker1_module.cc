7a7409454e4bbf36922e51345ced8a7c7b48e53e
Tingjun Yang <tjyang@fnal.gov>
2022-05-01 09:04:13 -0500

    Use SimEnergyDeposit information.

--- protoduneana/TutorialExamples/MoreExamples/BackTracker1_module.cc
+++ protoduneana/TutorialExamples/MoreExamples/BackTracker1_module.cc
@@ -20,6 +20,7 @@
 #include "larsim/MCCheater/BackTrackerService.h"
 #include "lardataobj/RecoBase/Track.h"
 #include "lardataobj/RecoBase/TrackHitMeta.h"
+#include "lardataobj/Simulation/SimEnergyDeposit.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "TH1D.h"
@@ -56,8 +57,9 @@ private:
   TH1D *hdeltaz;
 
   // number of points in 14 cm segments
-  TH1D *hsegpts_reco;
-  TH1D *hsegpts_true;
+  TH1D *hsegpts_reco; //using reconstructed information
+  TH1D *hsegpts_true; //using simchannel information
+  TH1D *hsegpts_sed; //using simenergydeposit information
 
 };
 
@@ -111,12 +113,13 @@ void pdsp::BackTracker1::analyze(art::Event const& e)
     double prevx_true = 0;
     double prevy_true = 0;
     double prevz_true = 0;
+    bool isfirst_reco = true;
+    bool isfirst_true = true;
     for (size_t ip = 0; ip<track->NPoints(); ++ip){
       // Ignore invalid trajectory points
       if (!track->HasValidPoint(ip)) continue;
       auto &loc = track->LocationAtPoint(ip);
       //find number of points in reco segments of 14 cm
-      static bool isfirst_reco = true;
       if (isfirst_reco){
         prevx_reco = loc.X();
         prevy_reco = loc.Y();
@@ -156,7 +159,6 @@ void pdsp::BackTracker1::analyze(art::Event const& e)
             hdeltay->Fill(loc.Y() - xyz[1]);
             hdeltaz->Fill(loc.Z() - xyz[2]);
             //find number of points in reco segments of 14 cm
-            static bool isfirst_true = true;
             if (isfirst_true){
               prevx_true = xyz[0];
               prevy_true = xyz[1];
@@ -185,7 +187,61 @@ void pdsp::BackTracker1::analyze(art::Event const& e)
         }
       }
     }
-  } 
+  }
+
+  // Get SimEnergyDeposit
+  std::vector<art::Ptr<sim::SimEnergyDeposit>> sedlist;
+  auto sedListHandle = e.getHandle< std::vector<sim::SimEnergyDeposit> >("largeant:LArG4DetectorServicevolTPCActive");
+  if (sedListHandle){
+    art::fill_ptr_vector(sedlist, sedListHandle);
+  }
+  else{
+    cout<<"sedListHandle invalid"<<endl;
+    return;
+  }
+
+  int nsegpts_sed = 0;
+  double seglength_sed = 0;
+  double prevx_sed = 0;
+  double prevy_sed = 0;
+  double prevz_sed = 0;
+  bool isfirst_sed = true;
+  for (size_t i = 0; i<sedlist.size(); ++i){
+    double x = sedlist[i]->X();
+    double y = sedlist[i]->Y();
+    double z = sedlist[i]->Z();
+    int trkid = sedlist[i]->TrackID();
+    //int pdg = sedlist[i]->PdgCode();
+    //cout<<x<<" "<<y<<" "<<z<<" "<<trkid<<" "<<pdg<<endl;
+    if (trkid!=1) continue;
+    if (isfirst_sed){
+      prevx_sed = x;
+      prevy_sed = y;
+      prevz_sed = z;
+      nsegpts_sed = 1;
+      isfirst_sed = false;
+    }
+    else{
+      double dis = sqrt(pow(x - prevx_sed, 2)+
+                        pow(y - prevy_sed, 2)+
+                        pow(z - prevz_sed, 2));
+      seglength_sed += dis;
+      if (seglength_sed>14){
+        hsegpts_sed->Fill(nsegpts_sed);
+        //cout<<"Fill sed segment "<<nsegpts_sed<<endl;
+//        if (nsegpts_sed<100){
+//          cout<<x<<" "<<y<<" "<<z<<" "<<prevx_sed<<" "<<prevy_sed<<" "<<prevz_sed<<" "<<nsegpts_sed<<endl;
+//        }
+        nsegpts_sed = 0;
+        seglength_sed = 0;
+      }
+      ++nsegpts_sed;
+      prevx_sed = x;
+      prevy_sed = y;
+      prevz_sed = z;
+    }
+    //cout<<"sed x "<<x<<" sed y "<<y<<" sed z "<<z<<" seglength_sed "<<seglength_sed<<" nsegpts_sed "<<nsegpts_sed<<endl;
+  }
 }
 
 void pdsp::BackTracker1::beginJob(){
@@ -196,6 +252,7 @@ void pdsp::BackTracker1::beginJob(){
   hdeltaz = tfs->make<TH1D>("hdeltaz", ";Reco z - true z (cm);N trajectory points",100,-2,2);
   hsegpts_reco = tfs->make<TH1D>("hsegpts_reco", "Reco segments;Number of points;Segments", 200,0,200);
   hsegpts_true = tfs->make<TH1D>("hsegpts_true", "True segments;Number of points;Segments", 200,0,200);
+  hsegpts_sed = tfs->make<TH1D>("hsegpts_sed", "SimEnergyDeposit segments;Number of points;Segments", 1000,0,1000);
 }
 
 DEFINE_ART_MODULE(pdsp::BackTracker1)
